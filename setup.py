08fb4ccc5ec339ede74bef5e94a15170eef476b7
MatheusGSantos <mths.gm@gmail.com>
2022-11-28 21:50:24 -0300

    Added api content

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='Django Rest API',
-    version='1.0',
-    description='Rest Api from django_rest_framework tutorial',
-    author='Matheus Gama dos Santos',
-    author_email='',
-    url='https://github.com/MatheusGSantos/django_rest_api_tutorial',
+    name="Django Rest API",
+    version="1.0",
+    description="Rest Api from django_rest_framework tutorial",
+    author="Matheus Gama dos Santos",
+    author_email="",
+    url="https://github.com/MatheusGSantos/django_rest_api_tutorial",
     packages=[],
-    install_requires=['Django', 'pylint', 'autopep8', 'djangorestframework']
+    install_requires=["Django", "pylint", "autopep8", "djangorestframework"],
 )
